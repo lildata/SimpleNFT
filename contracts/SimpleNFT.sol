87959a9933ddedde045f476f8a60348999560b20
lildata <lildata@users.noreply.github.com>
2020-04-18 16:26:11 +0200

    Added explicit relative oz contracts import

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
+
+//import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+//import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
 contract SimpleNFT is ERC721Full, ERC721Mintable {
     constructor() ERC721Full("Simple NFT", "SNFT") public {
